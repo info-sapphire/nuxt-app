d3ae6d4a3444fde090100d8472acf680db28dc0c
Sapphire <info-sapphire@yandex.ru>
2020-01-26 20:00:00 +0300

    feat(login): add login page

---
+++ client/pages/login.vue
@@ -0,0 +1,31 @@
+<template>
+  <ElContainer>
+    <ElCard shadow="always">
+      <ElForm ref="login">
+        <ElFormItem label="Логин">
+          <ElInput v-model.trim="controls.login" />
+        </ElFormItem>
+      </ElForm>
+    </ElCard>
+  </ElContainer>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  layout: 'empty',
+  data () {
+    return {
+      controls: {
+        login: '',
+        password: ''
+      }
+    }
+  },
+  created () {
+    this.$nuxt.$emit('chnage-background-color', '#202020')
+  }
+}
+</script>
+
+<style lang="scss" module></style>
