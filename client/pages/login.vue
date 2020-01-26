820ab03cd958fabf44d3eb1c098a70059aabdc61
Sapphire <info-sapphire@yandex.ru>
2020-01-26 20:13:15 +0300

    feat(login): update page
    added new input field
    removed labels
    added placeholders
    prefixes and suffixes added

--- client/pages/login.vue
+++ client/pages/login.vue
@@ -1,9 +1,33 @@
 <template>
-  <ElContainer>
-    <ElCard shadow="always">
+  <ElContainer :class="$style.container">
+    <ElCard
+      shadow="never"
+      :class="$style.card"
+    >
       <ElForm ref="login">
-        <ElFormItem label="Логин">
-          <ElInput v-model.trim="controls.login" />
+        <ElFormItem>
+          <ElInput
+            v-model.trim="controls.login"
+            prefix-icon="el-icon-user"
+            :placeholder="'Логин, email или телефон'"
+          >
+            <ElButton
+              slot="append"
+              icon="el-icon-info"
+            />
+          </ElInput>
+        </ElFormItem>
+        <ElFormItem>
+          <ElInput
+            v-model.trim="controls.passwd"
+            prefix-icon="el-icon-lock"
+            :placeholder="'Пароль'"
+          >
+            <ElButton
+              slot="append"
+              icon="el-icon-info"
+            />
+          </ElInput>
         </ElFormItem>
       </ElForm>
     </ElCard>
@@ -18,7 +42,7 @@ export default {
     return {
       controls: {
         login: '',
-        password: ''
+        passwd: ''
       }
     }
   },
@@ -28,4 +52,12 @@ export default {
 }
 </script>
 
-<style lang="scss" module></style>
+<style lang="scss" module>
+.container {
+  justify-content: center;
+}
+
+.card {
+  box-shadow: 0 0 200px rgba(255, 255, 255, 0.5), 0 1px 2px rgba(0, 0, 0, 0.3);
+}
+</style>
