b4e64542569f9e7a10731a9cb15c17d62e1245b8
Sapphire <info-sapphire@yandex.ru>
2020-01-26 19:18:38 +0300

    perf(element-ui): optimized styles

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,7 +17,7 @@ module.exports = {
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
   },
   loading: { color: '#fff' },
-  css: ['element-ui/lib/theme-chalk/index.css'],
+  css: [{ src: '~assets/scss/main.scss', lang: 'scss' }],
   plugins: ['@/plugins/element-ui'],
   buildModules: ['@nuxtjs/eslint-module'],
   modules: ['@nuxtjs/axios', ['@nuxtjs/dotenv', { path: __dirname }]],
