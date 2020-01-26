28bf89308e6123895f583818a2282e391cd5d7b8
Sapphire <info-sapphire@yandex.ru>
2020-01-26 19:24:21 +0300

    feat(layout): add empty

---
+++ client/layouts/empty.vue
@@ -0,0 +1,20 @@
+<template>
+  <div :class="$style.layout">
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Empty'
+}
+</script>
+
+<style lang="scss" module>
+.layout {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+}
+</style>
