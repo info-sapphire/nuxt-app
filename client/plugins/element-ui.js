35f6fe79b317aafa8fd2ddb7e1773c36fa38987d
Sapphire <info-sapphire@yandex.ru>
2020-01-26 19:10:14 +0300

    perf(element-ui): add used components

--- client/plugins/element-ui.js
+++ client/plugins/element-ui.js
@@ -1,5 +1,10 @@
 import Vue from 'vue'
-import Element from 'element-ui'
-import locale from 'element-ui/lib/locale/lang/en'
+import { Container, Aside, Main } from 'element-ui'
+import lang from 'element-ui/lib/locale/lang/ru-RU'
+import locale from 'element-ui/lib/locale'
 
-Vue.use(Element, { locale })
+locale.use(lang)
+
+Vue.use(Container)
+Vue.use(Aside)
+Vue.use(Main)
