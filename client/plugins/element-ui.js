98106513823d00193f500de785d0fc688fd13d66
Sapphire <info-sapphire@yandex.ru>
2020-01-26 19:51:12 +0300

    feat(element-ui): import components
    Card, Form, FormItem, Input

--- client/plugins/element-ui.js
+++ client/plugins/element-ui.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import { Container, Aside, Main } from 'element-ui'
+import { Container, Aside, Main, Card, Form, FormItem, Input } from 'element-ui'
 import lang from 'element-ui/lib/locale/lang/ru-RU'
 import locale from 'element-ui/lib/locale'
 
@@ -8,3 +8,7 @@ locale.use(lang)
 Vue.use(Container)
 Vue.use(Aside)
 Vue.use(Main)
+Vue.use(Card)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
