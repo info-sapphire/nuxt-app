4a6af884ac341f66373b0cf4df9cd87bc7b66b87
ex3xeng <ex3xeng@yandex.ru>
2020-01-27 17:03:16 +0300

    feat(loading): add demo full screen loading page

---
+++ client/pages/loading.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+  data: () => ({
+    loading: null
+  }),
+  beforeCreate () {
+    this.$nextTick(() => {
+      this.loading = this.$loading({ lock: true })
+    })
+  },
+  mounted () {
+    setTimeout(() => {
+      this.loading.close()
+      this.loading = null
+    }, 2000)
+  },
+  render () {
+    return true
+  }
+}
+</script>
