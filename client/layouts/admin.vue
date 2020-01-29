5b2b1a1bc8115205e955b439cceac1172ac59935
Sapphire <info-sapphire@yandex.ru>
2020-01-30 00:35:30 +0300

    feat: add admin layout

---
+++ client/layouts/admin.vue
@@ -0,0 +1,26 @@
+<template>
+  <div :class="$style.layout">
+    <ElContainer :class="$style.container">
+      <ElAside width="250px" />
+      <ElMain>
+        <Nuxt />
+      </ElMain>
+    </ElContainer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Admin'
+}
+</script>
+
+<style lang="scss" module>
+.layout {
+  width: 100%;
+  height: 100vh;
+}
+.container {
+  height: 100%;
+}
+</style>
