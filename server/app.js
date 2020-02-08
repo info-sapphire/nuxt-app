66baac7d26a02f689b67a552452062203c6e85a7
Sapphire <info-sapphire@yandex.ru>
2020-02-08 11:42:45 +0300

    feat: add body-parser

--- server/app.js
+++ server/app.js
@@ -1,6 +1,10 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 
 // init app
 const app = express()
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 
 module.exports = app
