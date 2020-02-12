ae2ce5122cace202c1651a9284a0a36426f55649
Sapphire <info-sapphire@yandex.ru>
2020-02-12 19:18:31 +0300

    feat: add CoddedError handler

---
+++ server/libraries/CodedError/codes.js
@@ -0,0 +1,26 @@
+const zeroCode = {
+  message: 'Unknown error',
+  status: 500
+}
+
+const codes = {}
+
+/**
+ *
+ * @param {number|string} code - Error code
+ * @returns {object} - Error
+ */
+const get = code => {
+  console.log(codes)
+  return code in codes ? { ...codes[code], code } : { ...zeroCode, code }
+}
+
+/**
+ *
+ * @param {object} input - Map error
+ */
+const use = (code, status, message) => {
+  codes[code] = { ...zeroCode, code, status, message }
+}
+
+module.exports = { get, use }
