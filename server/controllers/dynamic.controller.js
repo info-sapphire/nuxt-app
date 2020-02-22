f35fbcc88df23339a1cd9f67ad563e30f728eec6
Sapphire <info-sapphire@yandex.ru>
2020-02-22 15:22:37 +0300

    feat: add empty dynamic controller methods

---
+++ server/controllers/dynamic.controller.js
@@ -0,0 +1,5 @@
+module.exports.list = (req, res, next) => {}
+module.exports.tree = (req, res, next) => {}
+module.exports.create = (req, res, next) => {}
+module.exports.read = (req, res, next) => {}
+module.exports.update = (req, res, next) => {}
