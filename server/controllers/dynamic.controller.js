331394324bd209d6a3c7f550ff46e6e35afbbfe5
Sapphire <info-sapphire@yandex.ru>
2020-02-23 13:20:24 +0300

    polish: add new & rename method in dynamic controller

--- server/controllers/dynamic.controller.js
+++ server/controllers/dynamic.controller.js
@@ -1,5 +1,6 @@
 module.exports.list = (req, res, next) => {}
 module.exports.tree = (req, res, next) => {}
 module.exports.create = (req, res, next) => {}
-module.exports.read = (req, res, next) => {}
+module.exports.get = (req, res, next) => {}
 module.exports.update = (req, res, next) => {}
+module.exports.remove = (req, res, next) => {}
