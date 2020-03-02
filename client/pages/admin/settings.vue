ec9bcfdedf5038b4830fe187badc21c9570c8151
Sapphire <info-sapphire@yandex.ru>
2020-03-02 11:38:35 +0300

    feat(client): add AppPageHeadline component

--- client/pages/admin/settings.vue
+++ client/pages/admin/settings.vue
@@ -1,9 +1,36 @@
 <template>
-  <h1>hello from admin/settings</h1>
+  <div :class="$style.wrapper">
+    <AppPageHeadline
+      :breadcrumbs="breadcrumbs"
+      :name="page.name"
+    />
+  </div>
 </template>
 
 <script>
+import AppPageHeadline from '~/components/admin/page/AppPageHeadline'
+
 export default {
-  layout: 'admin'
+  layout: 'admin',
+
+  components: {
+    AppPageHeadline
+  },
+
+  data: () => ({
+    page: {
+      name: 'Настройки'
+    },
+
+    breadcrumbs: [
+      { name: 'Аналитика', link: '/admin' },
+      { name: 'Настройки', link: '' }
+    ]
+  })
 }
 </script>
+
+<style lang="scss" module>
+.wrapper {
+}
+</style>
