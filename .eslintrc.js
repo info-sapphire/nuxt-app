bdea360f8d0ed52617e9e0ac0a121663045387af
Sapphire <info.sapphire@yandex.ru>
2020-04-26 15:57:55 +0300

    workflow(code-style): add eslint rules

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+  ignorePatterns: ['node_modules/', 'client/assets/'],
+  extends: ['@nuxtjs', 'plugin:vue/recommended', '@vue/standard'],
+  rules: {
+    'vue/component-name-in-template-casing': [
+      'error',
+      'PascalCase',
+      { registeredComponentsOnly: false },
+    ],
+    'vue/max-attributes-per-line': [
+      'error',
+      {
+        singleline: 1,
+        multiline: {
+          max: 1,
+          allowFirstLine: false,
+        },
+      },
+    ],
+    'arrow-parens': 0,
+    'no-console': 0,
+    'import/order': 0,
+    'space-before-function-paren': 0,
+    'no-useless-catch': 0,
+    'vue/no-v-html': 0,
+  },
+}
